cd1c1366dfa653f272aaae0939033686b4d805a6
hanbyul1 <51542966+hanbyul1@users.noreply.github.com>
2026-04-13 22:08:30 -0400

    fix module type

--- server.js
+++ server.js
@@ -2,13 +2,34 @@ const express = require("express");
 
 const app = express();
 
+const fakeEbay = {
+    "123": [45, 50, 55],
+    "ABC123": [80, 90, 100]
+};
+
 app.get("/ping", (req, res) => {
     res.send("server alive");
 });
 
 app.get("/price", (req, res) => {
     const part = req.query.part;
-    res.send(`Looking up part: ${part}`);
+
+    const prices = fakeEbay[part];
+
+    if (!prices) {
+        return res.json({
+            part,
+            error: "No data"
+        });
+    }
+
+    const avg = prices.reduce((a, b) => a + b, 0) / prices.length;
+
+    res.json({
+        part,
+        avgPrice: avg,
+        samples: prices
+    });
 });
 
 const PORT = process.env.PORT || 3000;
